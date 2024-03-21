b628b1722c240939d44e3941ac5665ee070fd824
Courtney Truong <courtneybtruong@gmail.com>
2024-03-21 18:05:12 -0400

    Added Unit tests for GetInLibraryBooks and CreateBook Repo Methods

--- LibraryCatalogerTests/BookNUnitTests.cs
+++ LibraryCatalogerTests/BookNUnitTests.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing.Text;
 using static System.Reflection.Metadata.BlobBuilder;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryCatalogerTests;
 
@@ -17,18 +18,18 @@ namespace LibraryCatalogerTests;
 public class BookNUnitTests
 {
     private readonly Mock<IBookRepository> _bookRepositoryMock;
+    
 
     List<BookEntity> books = new List<BookEntity>
         {
-            new BookEntity { BookID = 1, Title = "TestBook1", Author = "TestAuthor", Description = "Test", InLibrary = true, ToBeReadList = true, Isbn = "12345678910" },
-            new BookEntity { BookID = 2, Title = "TestBook2", Author = "TestAuthor", Description = "Test", InLibrary = true, ToBeReadList = true, Isbn = "12345678910" },
-           
+           new BookEntity { BookID = 1, Title = "TestBook1", Author = "TestAuthor", Description = "Test", InLibrary = true, ToBeReadList = true, Isbn = "12345678910" },
+           new BookEntity { BookID = 2, Title = "TestBook2", Author = "TestAuthor", Description = "Test", InLibrary = true, ToBeReadList = true, Isbn = "12345678910" },  
         };
 
     public BookNUnitTests()
     {
 
-        _bookRepositoryMock = new Mock<IBookRepository>();
+       _bookRepositoryMock = new Mock<IBookRepository>();
        
     }
 
@@ -36,6 +37,7 @@ public class BookNUnitTests
     public void GetInLibraryBooks_ShouldReturnExpectedNumber()
     {
         //Arrange
+        
         var expected = 2;
 
         _bookRepositoryMock.Setup(u => u.GetInLibraryBooks()).Returns(books);
@@ -47,6 +49,40 @@ public class BookNUnitTests
         //Assert
         Assert.That(result.Count, Is.EqualTo(expected));
      }
+
+    [Test]
+    public void GetToBeReadList_ShouldReturnExpectedNumber()
+    {
+        //Arrange
+
+        var expected = 2;
+        _bookRepositoryMock.Setup(u => u.GetToBeReadList()).Returns(books);
+
+        //Act
+        List<BookEntity> result = _bookRepositoryMock.Object.GetToBeReadList();
+
+        //Assert
+        Assert.That(result.Count, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void CreateBook_ShouldReturnValidBookEntity()
+    {
+        //Arrange
+        var newbook = new BookEntity()
+        {
+            BookID = 1,
+            Title = "Test",
+            Author = "TestAuthor",
+            Description = "Test",
+            ToBeReadList = true,
+            InLibrary = true,
+        };
+        //Act
+        _bookRepositoryMock.Object.CreateBook(newbook);
+        //Assert
+        Assert.That(newbook, Is.TypeOf<BookEntity>());
+    }
 
 }
         
